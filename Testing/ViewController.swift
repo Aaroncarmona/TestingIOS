ab87c8bef6a17d55763ecbae533bea978e88f0f6
aaroncarmona <aaron.carmona@elektra.com.mx>
2019-07-29 14:00:06 -0500

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by aaroncarmona on 7/29/19.
+//  Copyright © 2019 aaroncarmona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
