c822cfb7ef1dc936563de2bfc90dacbe462e8239
aaroncarmona <aaron.carmona@elektra.com.mx>
2019-07-29 16:42:13 -0500

    init commit

---
+++ Testing/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Testing
+//
+//  Created by aaroncarmona on 7/29/19.
+//  Copyright © 2019 aaroncarmona. All rights reserved.
+//
+
+import Foundation
--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by aaroncarmona on 7/29/19.
 //  Copyright © 2019 aaroncarmona. All rights reserved.
 //
-
+import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
